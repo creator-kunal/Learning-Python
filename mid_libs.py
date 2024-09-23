b410b72de3b3bf09346bcd8435c3c8ca9a45f296
Kunal <kunal271202@gmail.com>
2024-09-23 15:38:56 +0530

    Create mid_libs.py
    A Python-based Mad Libs game that runs in the terminal. It prompts users for various words (nouns, verbs, adjectives) and inserts them into a pre-written story template, creating a humorous and often nonsensical narrative.

---
+++ mid_libs.py
@@ -0,0 +1,11 @@
+adjective = input("Enter an adejctive: ")
+noun = input("Enter a noun: ")
+adjective2 = input("Enter another adjective: ")
+verb = input("Enter a verb: ")
+adjective3 = input("Enter third adjective: ")
+
+
+print(f"Today I went to a {adjective} zoo.")
+print(f"IN an exhibit, I saw {noun}.")
+print(f"{noun} was {adjective2} and {verb}ing.")
+print(f"I was {adjective3}.")
